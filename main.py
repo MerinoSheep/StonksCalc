0ce51755077fcf818da3d50f075b859f19c9f98f
MerinoSheep <35940408+MerinoSheep@users.noreply.github.com>
2020-03-23 10:05:46 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from tkinter import ttk
 window = Tk()
 window.title("Stonks")
 #window.attributes('-zoomed', True) 
-#window.attributes("-fullscreen",True)
+window.attributes("-fullscreen",True)
 window.geometry("{0}x{1}+0+0".format(window.winfo_screenwidth(), window.winfo_screenheight()))
 window.mainloop()
